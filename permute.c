0256ce502ab55cdf3775296128bde4c8eb251c4b
Anthony Rabine <anthony@rabine.fr>
2022-05-11 14:25:37 +0200

    add permute.c example

---
+++ permute.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+ 
+ Write a function taking as parameters three real variables x, y, z
+
+
+Realize the circular permutation of these three variables: at the end of the function, x will contain the initial value of z, y will contain the initial value of x and z the initial value of y
+
+
+ This function will not return any value
+ 
+ */
+
+void permute(float *x, float *y, float *z)
+{
+    float tmp_x = *x;
+    float tmp_y = *y;
+    
+    *x = *z;
+    *z = tmp_y;
+    *y = tmp_x;
+}
+
+int main()
+{
+    float x = 3.14, y = 7.92, z = 10.01;
+    
+    printf("%.2f, %.2f, %.2f\n", x, y, z);
+    
+    permute(&x, &y, &z);
+
+    printf("%.2f, %.2f, %.2f\n", x, y, z);
+    return 0;
+}
